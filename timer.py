0f0e1998c82034ab6613bcf0708c2af1e3484fbd
cnthwng <145368830+cnthwng@users.noreply.github.com>
2023-10-06 10:51:08 -0400

    Update timer.py

--- timer.py
+++ timer.py
@@ -2,23 +2,22 @@
 This program is used for MGTC28 In-class Assignment 4.
 timer.py is a simple Python script that facilitates the Nerves of Steel game.
 Program will inform players of when to stand, and set a random timer between 5-25 seconds.
-Upon timer expiry, program will display text informing user time is up.
+Upon timer expiry, the program will display text informing the players time is up.
 timer.py uses the time library to help keep track of time.
-timer.py uses the random library to generate a random number within range.
+timer.py uses the random library to generate a random number within the range.
 """
 
 
-# This program is timer that counts down
+# This program is the timer that counts down
 
 
-import time # The time library has a sleep function that will pause the script for a specifized amount of time
-
-import random # The random library allows us to generate a random number between 5-25 seconds.
+import time # The time library has a sleep function that will pause the script for a specified amount of time
+import random # The random library allows us to generate a random number between 5-25 seconds
 
 print("Players stand") # Program to display text informing players to stand
 
-# ask user to enter desired countdown time
-set_time = random.randint(6, 24) # Random time between 5-25 seconds, therefore any number from 6-24.
+
+set_time = random.randint(6, 24) # Random time between 5-25 seconds, therefore any number from 6-24
 
 time.sleep(set_time) # Program sleeps duration of random time chosen
 
